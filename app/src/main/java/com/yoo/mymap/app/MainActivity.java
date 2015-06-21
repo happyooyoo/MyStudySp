4b1cada52c5332ec7bf7e6f355ce6a4441a9dfc6
yoo <1120466978@qq.com>
2015-06-21 22:05:51 +0800

    intial master branch

---
+++ app/src/main/java/com/yoo/mymap/app/MainActivity.java
@@ -0,0 +1,43 @@
+package com.yoo.mymap.app;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+
+
+public class MainActivity extends Activity {
+
+    private MapView mMapView;
+    private BaiduMap mBaiMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mMapView=(MapView)findViewById(R.id.baidu_map);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+}
